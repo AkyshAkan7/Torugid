aea56d5cf80c5a72aa9fee05c5254e2003b16362
Akan <23879@iitu.kz>
2019-09-27 08:13:47 +0600

    Initial Commit

---
+++ Tourgid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tourgid
+//
+//  Created by Akan Akysh on 9/27/19.
+//  Copyright © 2019 Akysh Akan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
